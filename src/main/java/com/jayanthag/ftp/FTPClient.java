fc9b4a050e5d177f6828e4136e32a9db0d7c77f9
Jay Gumballi <jayantha.gumballi@gmail.com>
2019-02-06 22:25:47 +0530

    setup gradle project

---
+++ src/main/java/com/jayanthag/ftp/FTPClient.java
@@ -0,0 +1,13 @@
+package com.jayanthag.ftp;
+
+public class FTPClient {
+
+    private String host;
+    private int port;
+
+    public FTPClient(String host, int port){
+        this.host = host;
+        this.port = port;
+    }
+
+}
